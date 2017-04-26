29f0ded0bb4d500b7e23b261b546db76085f84f0
benjongbloedt <bjongbloedt@users.noreply.github.com>
2017-04-26 08:54:44 -0500

    adding $(pwd) to symbolic link portion of the script

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -17,11 +17,11 @@ sudo apt-get install neovim -y
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.1/install.sh | bash
 
 # Symlink the configs
-ln -sf ./gitconfig ~/.gitconfig
-ln -sf ./gitignore ~/.gitignore
-ln -sf ./tmux/tmux.conf ~/.tmux.conf
-ln -sf ./ctags ~/.ctags
-ln -sf ./neovim/init.vim ~/.config/nvim/init.vim
-ln -sf ./zsh/zshrc ~/.zshrc
-ln -sf ./bash/bash_profile ~/.bash_profile
-ln -sf ./bash/bashrc ~/.bashrc
+ln -sf $(pwd)/gitconfig ~/.gitconfig
+ln -sf $(pwd)/gitignore ~/.gitignore
+ln -sf $(pwd)/tmux/tmux.conf ~/.tmux.conf
+ln -sf $(pwd)/ctags ~/.ctags
+ln -sf $(pwd)/neovim/init.vim ~/.config/nvim/init.vim
+ln -sf $(pwd)/zsh/zshrc ~/.zshrc
+ln -sf $(pwd)/bash/bash_profile ~/.bash_profile
+ln -sf $(pwd)/bash/bashrc ~/.bashrc
