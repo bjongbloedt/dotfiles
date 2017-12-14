a9ee9cf433c980610086ac67ef16b866a442dd6f
bjongbloedt <bjongbloedt@users.noreply.github.com>
2017-12-14 13:59:30 -0600

    adding nvm to mac install

--- install/mac.sh
+++ install/mac.sh
@@ -6,4 +6,8 @@
 # Install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
+# Setup default shell to zsh
 chsh -s /bin/zsh
+
+# Install nvm
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
