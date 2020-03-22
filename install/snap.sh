024e35c66486b4eef8f8545265e7465635761e3c
bjongbloedt <bjongbloedt@users.noreply.github.com>
2020-03-22 15:20:05 -0500

    moving to snaps

---
+++ install/snap.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Non classic installs
+snap install docker
+
+# The others
+snap install code --classic
+snap install pycharm-professional --classic
+snap install datagrip --classic
+snap install intellij-idea-ultimate --classic
+snap install tmux --classic
+snap install nvim --classic
